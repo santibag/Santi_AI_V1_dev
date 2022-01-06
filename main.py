1f7e238050dd4e053ef7ddce323d5338d0cfa3c9
Santibag <aerospace.salihku@gmail.com>
2022-01-06 17:25:43 +0300

    Code cleaned. Dictionary based memorization implemented.

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-# AI Memory is a list of answers and repetition counts
-AIMemory = []
+ai_memory = []
 
 
-# Checks if the answer is in the memory and tells if it is
-def evaluate_user_answer(user_answer, memory):
-    if user_answer in memory:
-        print("I know this!")
+# Checks if the answer is in the AI memory.
+# Returns a dictionary {"bool": False} if not inside
+# Returns the dictionary of the answer if inside
+def memory_check(answer, memory):
+    if len(memory) == 0:
+        return {"bool": False}
     else:
-        print("I don't know this")
+        for element in memory:
+            if element["name"] == answer:
+                return element
+    return {"bool": False}
 
 
-# Adds the answer to the AI memory
-# If the answer is a repeat, it increases the count, instead
-def memorize(answer, memory):
-    if answer not in memory:
-        memory.append(answer)  # The answer is memorized as a tuple of (Answer,repetition count)
-
-    # else:
-    #     print(AIMemory)
+# Adds the answer into the AI memory. Format is in the append below.
+def memorize_answer(answer, memory):
+    memory.append({"name": answer, "count": 1})
 
 
-#########################################################
-# The main loop below
-ProgramIsRunning = True
+# Decides what to do with the user answer. It's like the heart of the program.
+def answer_evaluation(answer, memory):
+    # Remember: memory_check() returns False if the answer is not known
+    # False return is checked first because the True return is not a bool but a dictionary return
+    answer_dictionary = memory_check(answer, memory)
+    if "bool" in answer_dictionary:
+        print("I don't know this.")
+        memorize_answer(answer, memory)
+    else:
+        print("I know this :D I've seen this {} times before!".format(answer_dictionary["count"]))
+        answer_dictionary["count"] += 1
 
-while ProgramIsRunning:
-    # Answer and evaluation
-    UserAnswer = input("Please write your message:\n")
-    evaluate_user_answer(UserAnswer, AIMemory)
 
-    # Memorizing the answer
-    memorize(UserAnswer, AIMemory)
-#########################################################
+working = True
+
+while working:
+    answer_evaluation(input("Please tell me something:\n"), ai_memory)
