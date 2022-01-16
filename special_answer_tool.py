26f5e3d64461bb9a12951fa123a9b82daded4a30
Santibag <aerospace.salihku@gmail.com>
2022-01-16 18:12:08 +0300

    Import protection added XD

--- special_answer_tool.py
+++ special_answer_tool.py
@@ -1,5 +1,9 @@
 """Lets you use a special answer function without running the AI"""
 
+if __name__ != "__main__":
+    print("Module import error! quit() applied for punishment XD")
+    quit()
+
 from answer_handler import special_answer
 
 # Never modify this, unless you have a reason! The tool runs just fine as it is!
