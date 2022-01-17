542d1fb7189b1180f6b3807e0d0ecd552316abe0
Santibag <aerospace.salihku@gmail.com>
2022-01-17 17:39:53 +0300

    Added the AI interface functions.

--- gui_handler.py
+++ gui_handler.py
@@ -2,19 +2,21 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 
-# from answer_handler import answer_evaluation
+from answer_handler import answer_evaluation
+# from file_handler import log_it
 
 
 class AiFace(Widget):
     label_text = StringProperty("Welcome!")
 
     def send_answer(self):
-        text = self.ids["text_input_1"].text
+        text = answer_evaluation(self.ids["text_input_1"].text)
         self.label_text = text
 
 
 class AiGuiApp(App):
     def build(self):
+        self.title = "Santi AI v1"
         return AiFace()
 
 
