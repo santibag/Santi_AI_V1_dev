f2880a59a4ece0de50136b801f5cf67f7ceaf9b6
Santibag <aerospace.salihku@gmail.com>
2022-01-17 17:39:53 +0300

    Log fixes and cleaning.

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from file_handler import log_it
 
 log_it("Log started!\n", initial=True)
-log_it("GUI being initialized.")
 
 # Possible values: "gui", "console"
 running_mode = "gui"
+log_it("Running mode: {}".format(running_mode))
+
 
 # If GUI mode is chosen, the console mode will be ignored and the AI will run in GUI mode.
 if running_mode == "gui":
+    log_it("Initializing GUI.")
     from gui_handler import start_gui
     start_gui()
 
+
 # If console mode is chosen, the loop below will start AI in console mode
 elif running_mode == "console":
+    log_it("Initializing console.")
+
     import answer_handler
+    answer_handler.running_mode = running_mode
+
     while True:
         # It will ask for a message
+
         user_answer = input("\nPlease tell me something:\n")
-        log_it("Input received: "+user_answer)
-        answer_handler.answer_evaluation(user_answer)
+        print(answer_handler.answer_evaluation(user_answer))
