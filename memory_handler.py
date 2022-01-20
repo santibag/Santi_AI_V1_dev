75f1062a1fd74945a0b7bd2dd764c35b8b109e90
Santibag <aerospace.salihku@gmail.com>
2022-01-20 14:09:12 +0300

    Cleaning

--- memory_handler.py
+++ memory_handler.py
@@ -7,29 +7,29 @@ memoryfile_name = "previous_user_inputs.aimind"
 ai_memory = file_handler.aiload(memoryfile_name)
 
 
-def memory_check(answer, memory=ai_memory):
-    """Checks if the answer is in the AI memory.\n
+def memory_check(message, memory=ai_memory):
+    """Checks if the message is in the AI memory.\n
     Returns a dictionary {"bool": False} if not inside\n
-    Returns the dictionary of the answer if inside"""
+    Returns the dictionary of the message if inside"""
     if len(memory) == 0:
         return {"bool": False}
     else:
         for element in memory:
-            if element["name"] == answer:
+            if element["name"] == message:
                 return element
     return {"bool": False}
 
 
-def memory_update(answer_dictionary, action, memory=ai_memory, memoryfile=memoryfile_name):
-    """Adds new answers into the AI memory as a dictionary. {"name": answer, "count": 1}\n
-    Updates counters of known answers\n
+def memory_update(message_dictionary, action, memory=ai_memory, memoryfile=memoryfile_name):
+    """Adds new messages into the AI memory as a dictionary. {"name": message, "count": 1}\n
+    Updates counters of known messages\n
     Pickles the memory into the memory file"""
     if action == "add":
-        memory.append(answer_dictionary)
+        memory.append(message_dictionary)
     if action == "update":
-        answer_dictionary["count"] += 1
-        answer_dictionary["last received"] = datetime.datetime.now().strftime("%X %x")
+        message_dictionary["count"] += 1
+        message_dictionary["last received"] = datetime.datetime.now().strftime("%X %x")
     if action == "remove":
-        memory.remove(answer_dictionary)
+        memory.remove(message_dictionary)
 
     file_handler.aisave(memory, memoryfile)
