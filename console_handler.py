9be8ef4998ec38fd27eb8faf0ad2daee60e7ee33
Santibag <aerospace.salihku@gmail.com>
2022-01-20 14:08:23 +0300

    Word for human input "answer" is changed to "message".

--- console_handler.py
+++ console_handler.py
@@ -1,10 +1,10 @@
 """This file runs the console mode of the app."""
-from conversation_handler import answer_evaluation
+from conversation_handler import message_evaluation
 
 
 def console_app():
     while True:
         # It will ask for a message
 
-        user_answer = input("\nPlease tell me something:\n")
-        print(answer_evaluation(user_answer))
+        user_message = input("\nPlease tell me something:\n")
+        print(message_evaluation(user_message))
--- conversation_handler.py
+++ conversation_handler.py
@@ -1,28 +1,32 @@
-"""Decides what to do with every received answer.
-Asks memory handler to save or provide the received answer's data."""
+"""Decides what to do with every received message.
+Asks memory handler to save or provide the received message's data."""
 from file_handler import log_it
 import memory_handler
 import datetime
 ai_memory = memory_handler.ai_memory
 
 
-def answer_evaluation(answer):
-    """Decides what to do with the user answer. It's like the heart of the program."""
-    log_it("Input received: " + answer)
-    answer_dictionary = memory_handler.memory_check(answer)
-    answer_time = datetime.datetime.now().strftime("%X %x")
-    if "bool" in answer_dictionary:
-        log_it("Unknown answer")
-        answer_dictionary = {"name": answer, "count": 1, "last received": answer_time}
-        memory_handler.memory_update(answer_dictionary, action="add")
-        log_it("Answer added to the memory.")
+def reply_human(result, message_dictionary):
+    reply_text = result
+    return result
+
+
+def message_evaluation(message):
+    """Decides what to do with the user message. It's like the heart of the program."""
+    log_it("Input received: " + message)
+    message_dictionary = memory_handler.memory_check(message)
+    message_time = datetime.datetime.now().strftime("%X %x")
+    if "bool" in message_dictionary:
+        log_it("Unknown message")
+        message_dictionary = {"name": message, "count": 1, "last received": message_time}
+        memory_handler.memory_update(message_dictionary, action="add")
+        log_it("message added to the memory.")
         result = "I don't know this."
-        log_it("Answer evaluation succeeded!\n")
-        return result
+        log_it("message evaluation succeeded!\n")
 
     else:
-        memory_handler.memory_update(answer_dictionary, action="update")
-        log_it("Known answer. Answer's counter increased to {}.".format(answer_dictionary["count"]))
-        result = "I know this :D I've seen this {} times before!".format(answer_dictionary["count"])
-        log_it("Answer evaluation succeeded!\n")
-        return result
+        memory_handler.memory_update(message_dictionary, action="update")
+        log_it("Known message. message's counter increased to {}.".format(message_dictionary["count"]))
+        result = "I know this :D I've seen this {} times before!".format(message_dictionary["count"])
+        log_it("message evaluation succeeded!\n")
+    return result
